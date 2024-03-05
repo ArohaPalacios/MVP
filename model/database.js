7418be938bfa78f9b7583bba680d4ff53c44636d
Aroha-Palacios-del-Pilar <aroha.palacios@gmail.com>
2024-03-05 14:36:03 +0100

    Initial Express commit

---
+++ model/database.js
@@ -0,0 +1,35 @@
+//This file creates a connection with the database.
+require("dotenv").config();
+const mysql = require("mysql");
+const fs = require("fs");
+
+//Set with which database I'll be working
+//when these variables are created, they are related to the data we have in .env. Make sure the names match.
+const DB_HOST = process.env.DB_HOST;
+const DB_USER = process.env.DB_USER;
+const DB_PASS = process.env.DB_PASS;
+const DB_NAME = process.env.DB_NAME;
+
+//create connection
+const con = mysql.createConnection({
+  host: DB_HOST || "127.0.0.1",
+  user: DB_USER || "root",
+  password: DB_PASS,
+  database: DB_NAME || "todos",
+  multipleStatements: true
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+
+  let sql = fs.readFileSync(__dirname + "/init_db.sql").toString();
+  con.query(sql, function(err, result) {
+    if (err) throw err;
+    console.log("Table creation `items` was successful!");
+
+    console.log("Closing...");
+  });
+
+  con.end();
+});
