9a1a226ad79d29ff9064efe71774bafcf242368d
Aroha-Palacios-del-Pilar <aroha.palacios@gmail.com>
2024-03-12 17:49:02 +0100

    add delete button in gallery

--- client/src/Gallery.jsx
+++ client/src/Gallery.jsx
@@ -3,15 +3,20 @@ import { useState } from "react";
 export default function({gallery}) {
 
     return (
-        <>
+        <div className="gallery_container">
         {gallery.map((image, index)=> (
+            <div className="img_container">
                 <img
                     className='optionImages'
                     key= {index}
-                    src={image.contentUrl}/> 
+                    src={image.contentUrl}/>
+            <button 
+                key= {index + "btn"}
+                className="dlt_btn">DLT</button>
+            </div>
         )) 
         }
-        </>
+        </div>
     )
 }
 
