22a3562c8e40dc4c94738cfd38662f8f50c9a7c2
Aroha-Palacios-del-Pilar <aroha.palacios@gmail.com>
2024-03-14 21:19:46 +0100

    change database

--- model/database.js
+++ model/database.js
@@ -26,7 +26,7 @@ con.connect(function(err) {
   let sql = fs.readFileSync(__dirname + "/init_db.sql").toString();
   con.query(sql, function(err, result) {
     if (err) throw err;
-    console.log("Table creation `concepts` and `sentence` was successful!");
+    console.log("Table creation `images` was successful!");
 
     console.log("Closing...");
   });
--- model/init_db.sql
+++ model/init_db.sql
@@ -4,7 +4,7 @@
 -- disable foreign key constraint checks. Allows to drop tables without checking for foreign keys constraints
 SET foreign_key_checks = 0;
 -- delete table if it exists. MySQL won't let me create a second table with the same name and it will throw an error. Deleting preexisting table prevents this error.
--- DROP TABLE if exists sentence;
+DROP TABLE if exists sentence;
 DROP TABLE if exists concepts;
 -- enable foreign key constraint checks. 
 SET foreign_key_checks = 1;
@@ -13,15 +13,11 @@ SET foreign_key_checks = 1;
 -- Create Tables
 --
 
--- CREATE TABLE `sentence`(
---     `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
---     `concepts_id` TEXT NOT NULL,
---     `words` TEXT NOT NULL
--- );
-CREATE TABLE `concepts`(
+CREATE TABLE `images`(
     `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `name` VARCHAR(255) NOT NULL,
-    `URL` VARCHAR(1024) NOT NULL
+    `search term` VARCHAR(255) NOT NULL,
+    `URL` BIGINT NOT NULL,
+    `type` VARCHAR(255) NULL,
+    `Concept_to_explain` VARCHAR(255) NOT NULL,
+    `order` BIGINT NULL
 );
--- ALTER TABLE
---     `sentence` ADD CONSTRAINT `sentence_concepts_id_foreign` FOREIGN KEY(`concepts_id`) REFERENCES `concepts`(`id`);
