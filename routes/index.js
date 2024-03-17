2ee5b1dca701cfd9f37e26d4f289492e927c572c
Aroha-Palacios-del-Pilar <aroha.palacios@gmail.com>
2024-03-17 08:26:42 +0100

    get images by sentences_id endpoint

--- routes/index.js
+++ routes/index.js
@@ -19,16 +19,28 @@ router.get("/api/images", async (req, res, next) => {
   }
 });
 
+router.get("/api/images/:sentences_id", async (req, res, next) => {
+  const{sentences_id} = req.params
+  const query = `SELECT * FROM images WHERE sentences_id=${sentences_id};`;
+  try {
+    const results = await db(query);
+    console.log("********", results.data);
+    res.send(results.data);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
 // INSERT a new image into the DB
 //Without error handling, when I send an empty object though Postman, this function still creates an image entry with correspondent id and undefined firstname and lastname.
 router.post("/api/images", async (req, res, next) => {
   //your code here
-  //I need a firstname and a lastname, that will be provided by the client in the req.body.
-  //Get firstname and lastname as variables by deconstructing req.body object.
+  //I need search_term, URL, type, concept, sentences_id, that will be provided by the client in the req.body.
+  //Get them as variables by destructuring req.body object.
   const { search_term, URL, type, concept, sentences_id } = req.body;
   const query = `INSERT INTO images (search_term, URL, type, concept, sentences_id) VALUES ('${search_term}', '${URL}','${type}','${concept}', '${sentences_id}');`;
   try {
-    //throw error if firstname or lastname are empty.
+    //throw error if search_term are empty.
     if (!search_term.length) {
       //set status of the error and send message to client.
       res.status(400).send({
@@ -45,13 +57,10 @@ router.post("/api/images", async (req, res, next) => {
 });
 
 router.post("/api/sentences", async (req, res, next) => {
-  //your code here
-  //I need a firstname and a lastname, that will be provided by the client in the req.body.
-  //Get firstname and lastname as variables by deconstructing req.body object.
   const { sentence} = req.body;
   const query = `INSERT INTO sentences (sentence) VALUES ('${sentence}');`;
   try {
-    //throw error if firstname or lastname are empty.
+    //throw error if sentence is empty.
     if (!sentence.length) {
       //set status of the error and send message to client.
       res.status(400).send({
@@ -68,8 +77,5 @@ router.post("/api/sentences", async (req, res, next) => {
 });
 
 module.exports = router;
-
-
-
 
 
