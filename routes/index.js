f3f16adc9034f21c89022bc750ee24f45e9bc25c
Aroha-Palacios-del-Pilar <aroha.palacios@gmail.com>
2024-03-17 07:04:21 +0100

    post endpoint for images table

--- routes/index.js
+++ routes/index.js
@@ -19,4 +19,29 @@ router.get("/api/images", async (req, res, next) => {
   }
 });
 
+// INSERT a new image into the DB
+//Without error handling, when I send an empty object though Postman, this function still creates an image entry with correspondent id and undefined firstname and lastname.
+router.post("/api/images", async (req, res, next) => {
+  //your code here
+  //I need a firstname and a lastname, that will be provided by the client in the req.body.
+  //Get firstname and lastname as variables by deconstructing req.body object.
+  const { search_term, URL, type, concept } = req.body;
+  const query = `INSERT INTO images (search_term, URL, type, concept) VALUES ('${search_term}', '${URL}','${type}','${concept}');`;
+  try {
+    //throw error if firstname or lastname are empty.
+    if (!search_term.length) {
+      //set status of the error and send message to client.
+      res.status(400).send({
+        message:
+          "please, provide a search_term in the correct format"
+      });
+    }
+    await db(query);
+    const results = await db(`SELECT * FROM images;`);
+    res.send(results.data);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
 module.exports = router;
