134058740444fcf804279dd50a7c46a0eea52872
Aroha-Palacios-del-Pilar <aroha.palacios@gmail.com>
2024-03-17 13:35:47 +0100

    move logic to home

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,52 +1,18 @@
 import { useState } from 'react'
 import './App.css'
 import { v4 as uuidv4 } from "uuid";
-import SearchImageView from "./components/SearchImageView.jsx"
-import OptionImages from "./components/OptionImages.jsx"
-import Gallery from "./components/Gallery.jsx"
+import Home from './components/Home.jsx';
 import Favorites from './components/Favorites.jsx';
 
 
+
 function App() {
-  const [optionImages, setOptionImages] = useState(null)
-  const [gallery, setGallery] = useState([])
 
-  //populate the state variable that holds the pictures shown as options to choose.
-  //the data comes from the SearchImageView component.
-  //This function is sent to it as props.
-  function populateImage(json) {
-    setOptionImages(json)
-  }
-
-  function handleAddToGallery(newImage) {
-    setGallery((state) => [...state, {...newImage,  id: uuidv4()}])
-  }
-
-  function handleDeleteFromGallery(id) {
-    setGallery(currentGallery=> {
-      return currentGallery.filter(image=> image.id !== id)
-    })
-  }
-
-  function handleClearGallery() {
-    setGallery([])
-  }
 
 
   return (
     <>
-    
-    <SearchImageView populateImage={(json)=>populateImage(json)}/>
-    {optionImages ? 
-      <OptionImages 
-        addToGallery= {(newImage)=> handleAddToGallery(newImage)}
-        images= {optionImages}/>: null}
-    {gallery.length ? 
-    <Gallery 
-      gallery = {gallery} 
-      handleClearGallery= {handleClearGallery}
-      deleteFromGallery={(id)=>handleDeleteFromGallery(id)}/> 
-      : null}
+    <Home/>
     <Favorites/>
     </>
   )
---
+++ client/src/components/Home.jsx
@@ -0,0 +1,50 @@
+import { useState, useEffect } from 'react'
+import { v4 as uuidv4 } from "uuid";
+import SearchImageView from "./SearchImageView.jsx"
+import OptionImages from "./OptionImages.jsx"
+import Gallery from "./Gallery.jsx"
+
+export default function() {
+    const [optionImages, setOptionImages] = useState(null)
+    const [gallery, setGallery] = useState([])
+  
+    //populate the state variable that holds the pictures shown as options to choose.
+    //the data comes from the SearchImageView component.
+    //This function is sent to it as props.
+    function populateImage(json) {
+      setOptionImages(json)
+    }
+  
+    function handleAddToGallery(newImage) {
+      setGallery((state) => [...state, {...newImage,  id: uuidv4()}])
+    }
+  
+    function handleDeleteFromGallery(id) {
+      setGallery(currentGallery=> {
+        return currentGallery.filter(image=> image.id !== id)
+      })
+    }
+  
+    function handleClearGallery() {
+      setGallery([])
+    }
+  
+  
+    return (
+      <>
+      
+      <SearchImageView populateImage={(json)=>populateImage(json)}/>
+      {optionImages ? 
+        <OptionImages 
+          addToGallery= {(newImage)=> handleAddToGallery(newImage)}
+          images= {optionImages}/>: null}
+      {gallery.length ? 
+      <Gallery 
+        gallery = {gallery} 
+        handleClearGallery= {handleClearGallery}
+        deleteFromGallery={(id)=>handleDeleteFromGallery(id)}/> 
+        : null}
+
+      </>
+    )
+}
