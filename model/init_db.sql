f3d9fd158d0a44e6c57f5c42f08428aa084f5b91
Aroha-Palacios-del-Pilar <aroha.palacios@gmail.com>
2024-03-17 06:26:44 +0100

    modify tables

--- model/init_db.sql
+++ model/init_db.sql
@@ -4,8 +4,8 @@
 -- disable foreign key constraint checks. Allows to drop tables without checking for foreign keys constraints
 SET foreign_key_checks = 0;
 -- delete table if it exists. MySQL won't let me create a second table with the same name and it will throw an error. Deleting preexisting table prevents this error.
-DROP TABLE if exists sentence;
-DROP TABLE if exists concepts;
+DROP TABLE if exists images;
+DROP TABLE if exists sentences;
 -- enable foreign key constraint checks. 
 SET foreign_key_checks = 1;
 
@@ -13,11 +13,17 @@ SET foreign_key_checks = 1;
 -- Create Tables
 --
 
+CREATE TABLE `sentences`(
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `sentence` TEXT NOT NULL,
+    `images_id` BIGINT UNSIGNED NOT NULL
+);
 CREATE TABLE `images`(
     `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `search term` VARCHAR(255) NOT NULL,
-    `URL` BIGINT NOT NULL,
+    `search_term` VARCHAR(255) NOT NULL,
+    `URL` TEXT NOT NULL,
     `type` VARCHAR(255) NULL,
-    `Concept_to_explain` VARCHAR(255) NOT NULL,
-    `order` BIGINT NULL
+    `concept` VARCHAR(255) NULL
 );
+ALTER TABLE
+    `sentences` ADD CONSTRAINT `sentences_images_id_foreign` FOREIGN KEY(`images_id`) REFERENCES `images`(`id`);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+const db = require("../model/helper");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.send({ title: 'Express' })
+});
+
+
+router.get("/api/images", async (req, res, next) => {
+  const query = "SELECT * FROM images;";
+  try {
+    const results = await db(query);
+    console.log("********", results.data);
+    res.send(results.data);
+  } catch (err) {
+    res.status(500).send(err);
+  }
 });
 
 module.exports = router;
