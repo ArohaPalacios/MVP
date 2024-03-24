acd7a0368e17246e501e2fb88a97ea0130312c81
Aroha-Palacios-del-Pilar <aroha.palacios@gmail.com>
2024-03-24 11:04:09 +0100

    navbar basic style

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,17 +1,16 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import {Link, NavLink} from "react-router-dom";
+
 
 export default function Navbar () {
     return(
-        <nav>
-      <ul>
-        <li>
-          <Link to= "/">Home </Link>
-        </li>
-        <li>
-          <Link to= "/favorites">Favorites</Link>
-        </li>
-      </ul>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary mb-3">
+        <div className="container-fluid">
+
+          <NavLink className="nav-link" activeclassname="active" to="/">Home</NavLink>
+          <NavLink className="nav-link" activeclassname="active" to="/favorites">Favorites</NavLink>
+          
+        </div>
     </nav>
     )
 }
