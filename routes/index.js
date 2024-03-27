1f5fe76e3eb05ad767a17aca7dce9984254d9221
Aroha-Palacios-del-Pilar <aroha.palacios@gmail.com>
2024-03-27 15:17:52 +0100

    update index.js comments.

--- routes/index.js
+++ routes/index.js
@@ -21,6 +21,7 @@ router.get("/api/sentences", async (req, res, next) => {
   }
 });
 
+//get images by sentences_id
 router.get("/api/images/:sentences_id", async (req, res, next) => {
   const{sentences_id} = req.params
   const query = `SELECT * FROM images WHERE sentences_id=${sentences_id};`;
@@ -33,6 +34,7 @@ router.get("/api/images/:sentences_id", async (req, res, next) => {
   }
 });
 
+//get sentence by id
 router.get("/api/sentences/:id", async (req, res, next) => {
   const{id} = req.params
   const query = `SELECT * FROM sentences WHERE id=${id};`;
@@ -45,13 +47,12 @@ router.get("/api/sentences/:id", async (req, res, next) => {
   }
 });
 
-
+//Post all the images in the gallery on one click.
 router.post("/api/images", async (req, res, next) => {
   console.log("Request Body:", req.body);
   try {
+    //will recieve the image gallery as an array of objects in the request body.
     const  images = req.body; 
-    // console.log(typeof images)
-    // console.log(images)
    // Check if images array is provided and not empty
     if (!Array.isArray(images) || images.length === 0) {
       return res.status(400).send({ message: "Please provide images in the correct format" });
@@ -63,11 +64,9 @@ router.post("/api/images", async (req, res, next) => {
       const query = `INSERT INTO images (search_term, URL, type, concept, sentences_id) VALUES ('${search_term}', '${URL}','${type}','${concept}', '${sentences_id}');`;
 
       // Execute the insert query one by one
-      console.log(query)
       await db(query);
-      
     }
-    // Get all images from the database
+    // Get all images from the database. this is not necessary for the app. This bit of code returns the images so that you can see them when you try it in Postman.
     const selectQuery = `SELECT * FROM images;`;
     const selectResult = await db(selectQuery);
 
@@ -79,13 +78,13 @@ router.post("/api/images", async (req, res, next) => {
   }
 });
 
+//post a new sentence in my sentences DB table and return the sentenceId as response.
 router.post("/api/sentences", async (req, res, next) => {
   const { sentence} = req.body;
   //Pass INSERT INTO and SELECT LAST_INSERT_ID() queries together 
   //make sure they're run exactly one after the other
   //otherwise the connection with the db is interrupted and I only get 0 as id.
   const query = `INSERT INTO sentences (sentence) VALUES ('${sentence}');SELECT LAST_INSERT_ID() AS id;`
-  //console.log( query)
   try {
     //throw error if sentence is empty.
     if (!sentence.length) {
@@ -96,10 +95,9 @@ router.post("/api/sentences", async (req, res, next) => {
       });
     }
     const results = await db(query);
-    console.log("*********///////////********" + results.data)
 
     // The results is an array holding an object with different keys.
-    // in that object, the key insertId holds the last id inserted
+    // in that object, the key "insertId" holds the last id inserted. I saw that console.logging the results
     const sentenceId = results.data[0].insertId;
     // send the ID of the newly inserted sentence as response
     res.status(201).send({ id: sentenceId });
